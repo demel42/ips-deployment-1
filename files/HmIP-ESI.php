05e25f0a57fb0a83f2dcb3d05ed8f33f925ecfdf
Ch. Damsky <demel42@web.de>
2024-01-11 17:29:06 +0100

    v1.5.1

--- files/HmIP-ESI.php
+++ files/HmIP-ESI.php
@@ -4,28 +4,6 @@ declare(strict_types=1);
 
 require_once IPS_GetScriptFile(MapLocalConstant('HELPER_GLOBAL'));
 
-$associations = [
-    ['Wert' => 0, 'Name' => 'unbekannt', 'Farbe' => -1],
-    ['Wert' => 1, 'Name' => 'Gas', 'Farbe' => -1],
-    ['Wert' => 2, 'Name' => 'LED', 'Farbe' => -1],
-    ['Wert' => 3, 'Name' => 'IEC', 'Farbe' => -1],
-    ['Wert' => 4, 'Name' => 'IEC/SML', 'Farbe' => -1],
-    ['Wert' => 5, 'Name' => 'IEC/SML WH', 'Farbe' => -1],
-    ['Wert' => 6, 'Name' => 'IEC/D0 Modus A', 'Farbe' => -1],
-    ['Wert' => 7, 'Name' => 'IEC/D0 Modus B', 'Farbe' => -1],
-    ['Wert' => 8, 'Name' => 'IEC/D0 Modus C', 'Farbe' => -1],
-    ['Wert' => 9, 'Name' => 'IEC/D0 Modus D', 'Farbe' => -1],
-];
-CreateVarProfile('HM.EnergyMeterOperationMode', VARIABLETYPE_INTEGER, '', 0, 0, 1, 0, '', $associations, true);
-
-$associations = [
-    ['Wert' => 0, 'Name' => 'normal', 'Farbe' => -1],
-    ['Wert' => 0, 'Name' => 'unbekannt', 'Farbe' => -1],
-    ['Wert' => 0, 'Name' => 'Überlauf', 'Farbe' => 0xEE0000],
-    ['Wert' => 0, 'Name' => 'Unterlauf', 'Farbe' => 0xEE0000],
-];
-CreateVarProfile('HM.EnergyMeterCounterStatus', VARIABLETYPE_INTEGER, '', 0, 0, 1, 0, '', $associations, true);
-
 // HmIP-ESI
 $setting = [
     'name' => 'Energiezähler',
